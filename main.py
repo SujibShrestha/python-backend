01bbd7de6575de68a3ae01b6e9c0ab94921c3588
SujibShrestha <sujeeb98@gmail.com>
2026-06-13 18:58:37 +0545

    Implemented http decorator

--- main.py
+++ main.py
@@ -7,10 +7,36 @@ def greet():
     return "Welcome"
 
 
-product=[
-    Product(id=1,name="phone",description="budget phone",price=11.2,quantity=1)
+products=[
+    Product(id=1,name="phone",description="budget phone",price=11.2,quantity=1),
+    Product(id=2,name="phone34",description="budget phone",price=11.2,quantity=1),
+    Product(id=3,name="phone23",description="budget phone",price=11.2,quantity=1)
 ]
 
 @app.get("/product")
-def all_product():
-    return product
+def all_product():  
+    return products
+
+@app.get("/product/{id}")
+def getProductById(id:int):
+    for product in products:
+     if product.id == id:
+        return product
+    return product[id]
+
+@app.post("/product")
+def addProduct(product:Product):
+    products.append(product)
+    return products
+
+@app.put("/product/{id}")
+def updateProduct(id:int,product:Product):
+   for i in range(len(products)):
+      if products[i].id== id :
+         products[i] = product
+         return "Added Successfully"
+    
+   return 'No product found'
+
+
+
